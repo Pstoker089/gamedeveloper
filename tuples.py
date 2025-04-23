51c366f52033e1599638ed244ee365d6adf8bf93
Pstoker089 <Penri.stoker@gmail.com>
2025-04-23 17:48:42 +0100

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,19 @@
+tuple1=("five",7,6.8,False)
+tuple=(2,4,3,1,"table",True)
+tuple2=(tuple1,tuple)
+#print(type(tuple))
+#print(tuple[2])
+#print(tuple2)
+
+tuple4=("chair",)*3
+#print(type(tuple4))
+
+#print(tuple[1:5])
+
+address=(32,"Queens road","Hartlepool","London","TS2736")
+(doornumb,streetname,townname,cityname,areacode)=address
+
+print(townname)
+
+#del tuple1
+
